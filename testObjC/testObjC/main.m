382753b2e2a38f15d3eb8ff700dcbef0b6724d3a
Tulip Gupta <tulipgupta@Guests-MacBook-Pro.local>
2015-05-25 20:25:58 -0700

    Adding testObjC

---
+++ testObjC/testObjC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testObjC
+//
+//  Created by Tulip Gupta on 5/25/15.
+//  Copyright (c) 2015 Tulip Gupta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Is there a white elephant in the room.\n");
+        NSLog(@"These hills are like white elephants.\n");
+    }
+    return 0;
+}
